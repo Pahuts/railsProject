1492f7f85b32a2baba3594801fb6cec4d37cc21c
Pahuts <luispahutanblock@gmail.com>
2021-02-13 21:54:18 +0800

    edited application.scss

--- app/assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-// ...
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
-//= require bootstrap-sprockets
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap'
+// bootstrap
+import "bootstrap"
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-
 // font awesome
 import "@fortawesome/fontawesome-free/js/all";
-import '../stylesheets/application.scss'
+import "../stylesheets/application.scss"
 
 
 
